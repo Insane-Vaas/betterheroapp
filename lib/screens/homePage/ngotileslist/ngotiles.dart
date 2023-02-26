f6ee547054eb53c4a4c79c7bae9a9bb42cc59775
Insane-Vaas <hrithikr200@gmail.com>
2023-02-26 18:33:16 +0530

    FutureBuilder

--- lib/screens/homePage/ngotileslist/ngotiles.dart
+++ lib/screens/homePage/ngotileslist/ngotiles.dart
@@ -1,5 +1,4 @@
-import 'dart:convert';
-
+import 'package:betterheroapp/httprequests/ngolist.dart';
 import 'package:betterheroapp/model/ngomodel/ngomodel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,40 +13,42 @@ class NGOTilesListWidget extends StatefulWidget {
 }
 
 class _NGOTilesListWidgetState extends State<NGOTilesListWidget> {
-  List<dynamic> list = [];
-
-  Future getNgoData() async {
-    final String x = await rootBundle.loadString('images/data.json');
-    final List data = await json.decode(x)['ngoData'];
-
-    setState(() {
-      list = data;
-    });
-  }
-
   @override
   void initState() {
-    getNgoData();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.all(8),
-      shrinkWrap: true,
-      physics: ClampingScrollPhysics(),
-      itemCount: list.length,
-      itemBuilder: (context, index) {
-        return NGOTileWidget(
-          ngoName: list[index]['ngoName'],
-          ngoCauses: list[index]['ngoCauses'],
-          ngoRating: list[index]['ngoRating'],
-          imgList: list[index]['ngoWorkingPhotos'],
-          ngoBio: list[index]['ngoBio'],
-          ngoLogoPic: list[index]['ngoLogoPhoto'],
-          ngoTeamPhoto: list[index]['ngoTeamPhotos'][0],
-        );
+    return FutureBuilder(
+      future: GetNGOList().getHomeNGOList(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            height: 200,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          return ListView.builder(
+            padding: EdgeInsets.all(8),
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return NGOTileWidget(
+                ngoName: snapshot.data![index]['ngoName'],
+                ngoCauses: snapshot.data![index]['ngoCauses'],
+                ngoRating: snapshot.data![index]['ngoRating'],
+                imgList: snapshot.data![index]['ngoWorkingPhotos'],
+                ngoBio: snapshot.data![index]['ngoBio'],
+                ngoLogoPic: snapshot.data![index]['ngoLogoPhoto'],
+                ngoTeamPhoto: snapshot.data![index]['ngoTeamPhotos'][0],
+              );
+            },
+          );
+        }
       },
     );
   }
@@ -99,8 +100,11 @@ class _NGOTileWidgetState extends State<NGOTileWidget> {
       },
       child: Container(
         decoration: ngoTileWidgetDecoration(),
-        margin: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.02,
-            15, MediaQuery.of(context).size.width * 0.02, 15),
+        margin: EdgeInsets.fromLTRB(
+            MediaQuery.of(context).size.width * 0.033,
+            MediaQuery.of(context).size.height * 0.01,
+            MediaQuery.of(context).size.width * 0.033,
+            MediaQuery.of(context).size.height * 0.01),
         height: MediaQuery.of(context).size.height * 0.31,
         width: MediaQuery.of(context).size.width * 0.9,
         child: Column(
@@ -108,11 +112,11 @@ class _NGOTileWidgetState extends State<NGOTileWidget> {
             ngoTileBackImageAndLike(
                 MediaQuery.of(context).size.height * 0.23, widget.imgList),
             ngoNameCauseAndRating(
-              widget.ngoName,
-              widget.ngoCauses,
-              widget.ngoRating,
-              MediaQuery.of(context).size.width * 0.9,
-            ),
+                widget.ngoName,
+                widget.ngoCauses,
+                widget.ngoRating,
+                MediaQuery.of(context).size.width * 0.9,
+                context),
           ],
         ),
       ),
@@ -226,27 +230,22 @@ BoxDecoration ngoTileWidgetDecoration() {
       color: Colors.grey.shade200);
 }
 
-Container ngoNameCauseAndRating(
-    String? ngoName, String? ngoCause, String? ngorating, double width) {
+Container ngoNameCauseAndRating(String? ngoName, String? ngoCause,
+    String? ngorating, double width, BuildContext context) {
   return Container(
-    width: width,
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ngoNameCause(
-          ngoName,
-          ngoCause,
-        ),
+        Expanded(child: ngoNameCause(ngoName, ngoCause, context)),
         ngoRating(ngorating),
       ],
     ),
   );
 }
 
-Container ngoNameCause(String? ngoName, String? ngoCause) {
+Container ngoNameCause(
+    String? ngoName, String? ngoCause, BuildContext context) {
   return Container(
-    width: 220,
+    // width: 220,
     margin: EdgeInsets.fromLTRB(8, 4, 0, 4),
     child: Column(
       mainAxisAlignment: MainAxisAlignment.start,
@@ -271,7 +270,7 @@ TextStyle ngoNameTextStyle() {
   return TextStyle(
     color: Colors.black,
     fontWeight: FontWeight.w600,
-    fontSize: 21,
+    fontSize: 18,
   );
 }
 
@@ -279,14 +278,14 @@ TextStyle ngoCauseTextStyle() {
   return TextStyle(
     color: Colors.grey.shade600,
     fontWeight: FontWeight.w400,
-    fontSize: 16,
+    fontSize: 12,
   );
 }
 
 Container ngoRating(String? ngorating) {
   return Container(
     decoration: BoxDecoration(
-      color: Colors.green,
+      color: Colors.orange.shade400,
       borderRadius: BorderRadius.circular(20),
     ),
     margin: EdgeInsets.fromLTRB(8, 4, 10, 4),
